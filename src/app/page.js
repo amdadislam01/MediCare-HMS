bc4b7210def30a37bfe69ceb0216c7271a6324ee
ashikurahman1 <ashikurahmantuhin@gmail.com>
2025-12-26 20:18:16 +0600

    add: added the Hero and Medical Department

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,9 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="my-10">
+    <div className="">
       <HeroSection />
-
-      <p>Hello</p>
+      <MedicalDepartments />
     </div>
   );
 }
