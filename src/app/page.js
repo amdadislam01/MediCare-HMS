1d104da87d98ba60fb5fa347173c9c16e94afbf3
amdadisalm01 <amdadislam733@gmail.com>
2025-12-26 21:04:48 +0600

    Services section complate

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import HeroSection from '@/components/HomePage/HeroSection/HeroSection';
-import Image from 'next/image';
+import MedicalDepartments from '@/components/MedicalDepartments/MedicalDepartments';
+import Services from '@/components/Services/Services';
 
 export default function Home() {
   return (
     <div className="">
       <HeroSection />
+      <Services />
       <MedicalDepartments />
     </div>
   );
